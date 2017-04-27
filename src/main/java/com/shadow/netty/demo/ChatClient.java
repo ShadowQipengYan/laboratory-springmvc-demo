898cd83f3dd4a0b9858488f67263021c6c4ef953
ShadowQipengYan <724184115@qq.com>
2017-04-27 23:42:30 +0800

    netty

--- src/main/java/com/shadow/netty/demo/ChatClient.java
+++ src/main/java/com/shadow/netty/demo/ChatClient.java
@@ -25,171 +25,171 @@ import java.net.UnknownHostException;
 import javax.net.ssl.SSLException;
 import javax.swing.JOptionPane;
 /*
- * øÕªß∂À
- * ¥”¥À∆Ù∂ØøÕªß∂À
+ * ÂÆ¢Êà∑Á´Ø
+ * ‰ªéÊ≠§ÂêØÂä®ÂÆ¢Êà∑Á´Ø
  */
 
 public class ChatClient
 {
 
-	static final String HOST = System.getProperty("host", "127.0.0.1");
-	static final int PORT = Integer.parseInt(System.getProperty("port", "8992"));
-	static private String loginName="";
-	
-	public static String getName(){
-		return loginName;
-	}
-	
-	static java.awt.List cList = new java.awt.List(6);
-	TextField chatField = new TextField(45);
-	
-	TextField tfData = new TextField(20);
-	Label label=new Label("«Î ‰»Îƒ⁄»›");
-	Button btnSend=new Button("∑¢ÀÕ");
-	
-	static Channel ch = null;
-	static EventLoopGroup clientGroup = null;
+    static final String HOST = System.getProperty("host", "127.0.0.1");
+    static final int PORT = Integer.parseInt(System.getProperty("port", "8992"));
+    static private String loginName="";
+
+    public static String getName(){
+        return loginName;
+    }
+
+    static java.awt.List cList = new java.awt.List(6);
+    TextField chatField = new TextField(45);
+
+    TextField tfData = new TextField(20);
+    Label label=new Label("ËØ∑ËæìÂÖ•ÂÜÖÂÆπ");
+    Button btnSend=new Button("ÂèëÈÄÅ");
+
+    static Channel ch = null;
+    static EventLoopGroup clientGroup = null;
 
 	/*
-	 * …˙≥…¥∞ÃÂ
+	 * ÁîüÊàêÁ™ó‰Ωì
 	 */
 
-	void showFrame()
-	{
-		
-		Frame cFrame = new Frame("CallMe");
-		cFrame.setSize(500, 350);
-		cFrame.setVisible(true);
-		cFrame.setLocationRelativeTo(null);
-		cFrame.setResizable(false);
-		cFrame.addWindowListener(new WindowAdapter()
-		{
+    void showFrame()
+    {
 
-			@Override
-			public void windowClosing(WindowEvent e)
-			{
-				clientGroup.shutdownGracefully();
-				System.exit(0);
-			}
+        Frame cFrame = new Frame("CallMe");
+        cFrame.setSize(500, 350);
+        cFrame.setVisible(true);
+        cFrame.setLocationRelativeTo(null);
+        cFrame.setResizable(false);
+        cFrame.addWindowListener(new WindowAdapter()
+        {
 
-		});
-		
-	
-		cFrame.add(cList, "Center");
-		Panel p = new Panel();
-		//p.setLayout(new BorderLayout());
-		p.add(label);
-		p.add(chatField);
-		p.add(btnSend);
-		cFrame.add(p, "South");
+            @Override
+            public void windowClosing(WindowEvent e)
+            {
+                clientGroup.shutdownGracefully();
+                System.exit(0);
+            }
 
-		chatField.addActionListener(new ActionListener()
-		{
+        });
 
-			@Override
-			public void actionPerformed(ActionEvent e)
-			{
 
-				try
-				{
-	
-					if (!chatField.getText().trim().equals(""))
-					{
-						String line = "["+loginName+"]"+chatField.getText();
+        cFrame.add(cList, "Center");
+        Panel p = new Panel();
+        //p.setLayout(new BorderLayout());
+        p.add(label);
+        p.add(chatField);
+        p.add(btnSend);
+        cFrame.add(p, "South");
 
-						ChannelFuture lastWriteFuture = null;
-						lastWriteFuture = ch.writeAndFlush(line + "\r\n");
-		
-						chatField.setText("");
+        chatField.addActionListener(new ActionListener()
+        {
 
-						if (line.equalsIgnoreCase("bey"))
-						{
-							ch.closeFuture().sync();
-						}
+            @Override
+            public void actionPerformed(ActionEvent e)
+            {
 
-						if (lastWriteFuture != null)
-						{
-							lastWriteFuture.sync();
-						}
-					}
-				} catch (InterruptedException e3)
-				{
-					// TODO Auto-generated catch block
-					e3.printStackTrace();
-				} finally
-				{
-					//clientGroup.shutdownGracefully();
-				}
+                try
+                {
 
-			}
-		});
+                    if (!chatField.getText().trim().equals(""))
+                    {
+                        String line = "["+loginName+"]"+chatField.getText();
 
-		
-		btnSend.addActionListener(new ActionListener()
-		{
+                        ChannelFuture lastWriteFuture = null;
+                        lastWriteFuture = ch.writeAndFlush(line + "\r\n");
 
-			@Override
-			public void actionPerformed(ActionEvent e)
-			{
-				try
-				{	
-					if (!chatField.getText().trim().equals(""))
-					{
-						String line = "["+loginName+"]"+chatField.getText();
-						System.out.println(line);
-						ChannelFuture lastWriteFuture = null;
-						lastWriteFuture = ch.writeAndFlush(line + "\r\n");
-		
-						chatField.setText("");
+                        chatField.setText("");
 
-						if (line.equalsIgnoreCase("bey"))
-						{
-							ch.closeFuture().sync();
-						}
-						if (lastWriteFuture != null)
-						{
-							lastWriteFuture.sync();
-						}
-					}
-				} catch (InterruptedException e3)
-				{
-					// TODO Auto-generated catch block
-					e3.printStackTrace();
-				} finally
-				{
-					//clientGroup.shutdownGracefully();
-				}
+                        if (line.equalsIgnoreCase("bey"))
+                        {
+                            ch.closeFuture().sync();
+                        }
 
-			}
-		});
+                        if (lastWriteFuture != null)
+                        {
+                            lastWriteFuture.sync();
+                        }
+                    }
+                } catch (InterruptedException e3)
+                {
+                    // TODO Auto-generated catch block
+                    e3.printStackTrace();
+                } finally
+                {
+                    //clientGroup.shutdownGracefully();
+                }
 
-		
+            }
+        });
 
-	}
 
-	public static void main(String[] args) throws SSLException, InterruptedException
-	{		
-		try {	
-			//∏∫‘Ω” ‹“ª∏ˆ”√ªßƒ¨»œµƒ”√ªß√˚
-			String a=JOptionPane.showInputDialog("Input your nickname please£°");
-			if(a!=null){
-				if(!a.trim().equals(""))loginName=a;
-				else {
-					loginName=InetAddress.getLocalHost().toString();
-					System.out.println(loginName);
-				}
-			}
-		} catch (UnknownHostException e) {
-			// TODO Auto-generated catch block
-			
-			e.printStackTrace();
-		}		
-		final SslContext sslctx = SslContext.newClientContext(InsecureTrustManagerFactory.INSTANCE);
-		clientGroup = new NioEventLoopGroup();
-		Bootstrap b = new Bootstrap();
-		b.group(clientGroup).channel(NioSocketChannel.class).handler(new SecureChatClientInitializer(sslctx));
-		ch = b.connect(HOST, PORT).sync().channel();
-		new ChatClient().showFrame();
-	}
+        btnSend.addActionListener(new ActionListener()
+        {
+
+            @Override
+            public void actionPerformed(ActionEvent e)
+            {
+                try
+                {
+                    if (!chatField.getText().trim().equals(""))
+                    {
+                        String line = "["+loginName+"]"+chatField.getText();
+                        System.out.println(line);
+                        ChannelFuture lastWriteFuture = null;
+                        lastWriteFuture = ch.writeAndFlush(line + "\r\n");
+
+                        chatField.setText("");
+
+                        if (line.equalsIgnoreCase("bey"))
+                        {
+                            ch.closeFuture().sync();
+                        }
+                        if (lastWriteFuture != null)
+                        {
+                            lastWriteFuture.sync();
+                        }
+                    }
+                } catch (InterruptedException e3)
+                {
+                    // TODO Auto-generated catch block
+                    e3.printStackTrace();
+                } finally
+                {
+                    //clientGroup.shutdownGracefully();
+                }
+
+            }
+        });
+
+
+
+    }
+
+    public static void main(String[] args) throws SSLException, InterruptedException
+    {
+        try {
+            //Ë¥üË¥£Êé•Âèó‰∏Ä‰∏™Áî®Êà∑ÈªòËÆ§ÁöÑÁî®Êà∑Âêç
+            String a=JOptionPane.showInputDialog("Input your nickname pleaseÔºÅ");
+            if(a!=null){
+                if(!a.trim().equals(""))loginName=a;
+                else {
+                    loginName=InetAddress.getLocalHost().toString();
+                    System.out.println(loginName);
+                }
+            }
+        } catch (UnknownHostException e) {
+            // TODO Auto-generated catch block
+
+            e.printStackTrace();
+        }
+        final SslContext sslctx = SslContext.newClientContext(InsecureTrustManagerFactory.INSTANCE);
+        clientGroup = new NioEventLoopGroup();
+        Bootstrap b = new Bootstrap();
+        b.group(clientGroup).channel(NioSocketChannel.class).handler(new SecureChatClientInitializer(sslctx));
+        ch = b.connect(HOST, PORT).sync().channel();
+        new ChatClient().showFrame();
+    }
 
 }
